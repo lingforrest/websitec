528c80e1df281ca16da2e6cf19c51e8f879d954b
lingforrest <lingforrest@icloud.com>
2016-02-13 18:47:17 +0800

    Create common_pages.c

---
+++ common_pages.c
@@ -0,0 +1,34 @@
+const char* g_page_start = 
+"<!DOCTYPE html>\n"
+"<html>\n"
+"<head>\n"
+"<meta name=\"name\" content=\"openhpc\" lang=\"zh_CN\" charset=\"UTF-8\" />\n"
+"<style type=\"text/css\">"
+"body {"
+"color:white;"
+"background-color:blue;" 
+"}"
+"</style>"
+"<title>OpenHPC 开放高性能计算</title>\n"
+"</head>\n"
+"<body id=\"body\">\n";
+
+const char* g_page_menu_admin =
+"<ul id=\"menu\"> 菜单 \n"
+"<li><a href=\"/admin/home\">我的主页</a></li>\n"
+"<li><a href=\"/admin/post\">发布文章</a></li>\n"
+"<li><a href=\"/admin/logout\">退出账户</a></li>\n"
+"</ul>\n"
+"<div id=\"content\">管理内容</div>\n";
+
+const char* g_page_menu_pub =
+"<ul id=\"menu\"> 菜单 \n"
+"<li><a href=\"/home\">我的主页</a></li>\n"
+"<li><a href=\"/login\">登录</a></li>\n"
+"<li><a href=\"/reg\">注册</a></li>\n"
+"</ul>\n"
+"<div id=\"content\">管理内容</div>\n";
+
+const char* g_page_end =
+"</body>\n"
+"</html>\n";
